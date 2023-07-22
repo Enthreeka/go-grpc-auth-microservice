6bbe4c6be91b9d79ef1c0baff56595ae59bbfabb
Enthreeka <n3ksmirnov@gmail.com>
2023-07-22 17:15:35 +0300

    feat: new entity user and interface for redis v1

---
+++ internal/repo/redis/storage.go
@@ -0,0 +1,12 @@
+package redis
+
+import (
+	"context"
+	"github.com/NASandGAP/auth-microservice/internal/entity"
+)
+
+type Repository interface {
+	GetUserByID(ctx context.Context, id string) (entity.User, error)
+	DeleteUserByID(ctx context.Context, id string) error
+	CreateUser(ctx context.Context, user entity.User) error
+}
